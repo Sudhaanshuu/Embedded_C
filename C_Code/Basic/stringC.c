f2490591afe5e28b99746675eb8aa0c3069aa5bb
Sudhanshu <125084256+Sudhaanshuu@users.noreply.github.com>
2024-07-05 13:31:06 +0530

    //? C program for the taking and printing he whole line of the input

---
+++ C_Code/Basic/stringC.c
@@ -0,0 +1,14 @@
+#include<STDIO.H>
+//? C program for the taking and printing he whole line of the input 
+int main(){
+char suku[60];
+// printf("Enter your name");
+// scanf("%[^\n]%*c",&suku);
+
+// printf("name %s",suku);
+
+
+int s=418;
+printf("%p",&s);
+    return 0;
+}
