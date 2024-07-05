5b663e92e0c982e1bfb98bd62fb55000c03a9adb
Sudhanshu <125084256+Sudhaanshuu@users.noreply.github.com>
2024-07-05 13:16:32 +0530

    //! Cprogram for the String pring thing with the marging and tab spaces

---
+++ C_Code/Basic/Formatted.c
@@ -0,0 +1,13 @@
+#include<STDIO.H>
+//! Cprogram for the String pring thing with the marging and tab spaces 
+int main(){
+char su[]="Hlo Sudhanshu";
+
+printf("%20s\n",su);
+printf("%-20s\n",su);
+
+printf("%20.5s\n",su);
+printf("%-20.5s\n",su);
+
+    return 0;
+}
