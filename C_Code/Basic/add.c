908b70dda386a323658bab700d2ed724ca4d8a6c
Sudhanshu <125084256+Sudhaanshuu@users.noreply.github.com>
2024-07-05 12:58:32 +0530

    Add the C program for the Sum of two Integers

---
+++ C_Code/Basic/add.c
@@ -0,0 +1,16 @@
+#include<STDIO.H>
+
+int main()
+{
+//& Sum
+    printf("Enter first  number");
+    int a;
+    scanf("%d",&a);
+    printf("Enter second number");
+    int b;
+    scanf("%d",&b);
+    int c= a+b;
+    printf("%d",c);
+
+    return 0;
+}
