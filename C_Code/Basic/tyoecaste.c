d68404f68e7b2f271b0b1da782648d1ab2fb0d5d
Sudhanshu <125084256+Sudhaanshuu@users.noreply.github.com>
2024-07-05 13:33:16 +0530

    //? Simplest c program for the typecsating

---
+++ C_Code/Basic/tyoecaste.c
@@ -0,0 +1,13 @@
+#include<STDIO.H>
+//? Simplest c program for the typecsating 
+int main(int argc, char const *argv[])
+{
+    int s=5;
+    float k=6.32;
+    char p=3;
+    s=k+p;
+
+    printf("%d\n %f\n %c\n ",s,k,p);
+
+    return 0;
+}
