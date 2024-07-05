3e4ec9d5fc4b38bb524a5abe75a7e3fd69d22cb2
Sudhanshu <125084256+Sudhaanshuu@users.noreply.github.com>
2024-07-05 13:29:28 +0530

    //? C program for the embadded logic of set a bit , clear a boit and loggle a bit

---
+++ C_Code/Basic/signCheck.c
@@ -0,0 +1,39 @@
+#include<STDIO.H>
+#include<LIMITS.H>
+
+int main()
+{
+
+    // int suku= INT_MAX;
+    // int sukuu= INT_MIN;
+    // printf("%d TO %d",suku,sukuu);
+
+    
+
+
+
+    // unsigned int s1=0;
+    // unsigned int s2=UINT_MAX;
+    //   printf("%u TO %u",s1,s2);
+
+
+    
+    //  short s1=SHRT_MIN;
+    //  short s2=SHRT_MAX;
+    // printf("%d TO %d",s1,s2);
+
+
+
+    // short unsigned  s1=0;
+    // short unsigned  s2=USHRT_MAX;
+
+    // printf("%d TO %d",s1,s2);
+
+    // printf("%d",sizeof(short));
+
+
+
+
+
+    return 0;
+}
