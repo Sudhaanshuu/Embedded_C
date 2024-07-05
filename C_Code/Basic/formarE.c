3233666cbcd410353dd7d3e0e74cc69484439ae0
Sudhanshu <125084256+Sudhaanshuu@users.noreply.github.com>
2024-07-05 13:13:41 +0530

    C Program for checking the formation of the e , E and f

---
+++ C_Code/Basic/formarE.c
@@ -0,0 +1,10 @@
+#include<STDIO.H>
+//! Program for checking the formation of the e , E and f
+int main(){
+float s=12.67;
+printf("Using the %%f %f\n",s);
+printf("Using the %%e %e\n",s);
+printf("Using the %%E %E\n",s);
+
+    return 0;
+}
