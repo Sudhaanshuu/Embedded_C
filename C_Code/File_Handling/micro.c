f6d5ce7d4c418dc947923aa4ccd233e0a045fc7a
Sudhanshu <125084256+Sudhaanshuu@users.noreply.github.com>
2024-07-05 13:40:29 +0530

    //! C program for the Micros

---
+++ C_Code/File_Handling/micro.c
@@ -0,0 +1,22 @@
+#include<STDIO.H>
+
+//! C program for the Micros 
+#define suku 418
+#define Hi printf("\nSudhanshu ")
+#define suku5 scanf("%d",&j)
+
+#define sum(a,b) (a+b)
+
+#define vashu printf("Working")
+int main()
+{
+    int j;
+    suku5;
+    printf("%d",j);
+    printf("%d",suku);
+    Hi;
+    vashu;
+   int k= sum(2,4);
+   printf("%d",k);
+    return 0;
+}
