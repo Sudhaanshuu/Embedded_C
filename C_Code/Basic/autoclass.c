69d711c15a65b6a209af4e2c4e70e3ac4d6178f9
Sudhanshu <125084256+Sudhaanshuu@users.noreply.github.com>
2024-07-05 13:03:46 +0530

    Add a C program for the checking which variable is accessable

---
+++ C_Code/Basic/autoclass.c
@@ -0,0 +1,17 @@
+#include<STDIO.H>
+//? program for the checking which variable is accessable
+
+void suku(){
+    printf("Love You ");
+    auto int a=416;
+    printf("%d",a);
+    a=a+7;
+}
+
+int main()
+{
+    suku();
+
+    // printf("%d",a);
+    return 0;
+}
