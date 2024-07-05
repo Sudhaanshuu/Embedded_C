325ccae3bf4e00430edd54cb35f6f668e8b02505
Sudhanshu <125084256+Sudhaanshuu@users.noreply.github.com>
2024-07-05 13:01:30 +0530

    Add the C program for the BitWise opration And OR NOT EXOR

---
+++ C_Code/Basic/Bitwise.c
@@ -0,0 +1,19 @@
+#include<STDIO.H>
+
+int main(){
+
+
+int s=35,k=9;
+// printf("%d\n",s & k);//? AND
+// printf("%d\n",s | k);//! OR
+// printf("%d\n",s ^ k);//* EXOR
+
+printf("%d\n",~s);//& 2's Compliment
+
+printf("%d\n",~-12);
+
+
+// printf("%d\n",5>>1);//! Right Shift
+// printf("%d\n",5<<2);//* left shift
+    return 0;
+}
