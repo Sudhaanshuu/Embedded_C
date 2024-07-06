348bf3f6d7dfa20cd4c37202a6d84cec4ae97500
Sudhanshu <125084256+Sudhaanshuu@users.noreply.github.com>
2024-07-06 13:01:10 +0530

    //* C Program for the maching and extrating the target valve

---
+++ C_Code/Arrays/targt.c
@@ -0,0 +1,24 @@
+#include<STDIO.H>
+//* C Program for the maching and extrating the target valve
+int main(int argc, char const *argv[])
+{
+    
+    int su[10];
+    for (int i = 0; i < 5; i++)
+    {
+       printf("Enter Element of the array at  %d",i);
+    scanf("%d",&su[i]);
+    }
+    int t=15;
+    int sum=0;
+    for (int i = 0; i < 5; i++)
+    {
+        if (sum<t)
+        {
+            /* code */
+        }
+        
+    }
+    
+    return 0;
+}
