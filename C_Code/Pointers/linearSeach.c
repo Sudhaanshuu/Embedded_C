c3f6e1cd8777973deeb79165e92a5b8d40c6eeb0
Vashu <iamsudhan.shuu@gmail.com>
2024-07-07 20:26:46 +0530

    Add found variable in  linearSeach.c
    Added a found variable to track if the element was found.

--- C_Code/Pointers/linearSeach.c
+++ C_Code/Pointers/linearSeach.c
@@ -1,23 +1,23 @@
-#include<STDIO.H>
-// * C Program to perform liner seach using the pointers
-int main(){
-int suku[]={1,3,4,6,8};
+#include <stdio.h>
 
-int tar=6;
-int *p;
-p=&tar;
+// C Program to perform linear search using pointers
+int main() {
+    int suku[] = {1, 3, 4, 6, 8};
+    int tar = 6;
+    int *p = &tar;
+    int found = 0; 
 
-for (int i = 0; i < 5; i++)
-{
-   if (suku[i]==*p)
-   {
-   printf("Element fount at %d",i);
-   }
-   
-}
+    for (int i = 0; i < 5; i++) {
+        if (suku[i] == *p) {
+            printf("Element found at index %d\n", i);
+            found = 1;
+            break;
+        }
+    }
 
-
+    if (!found) {
+        printf("Element not found\n");
+    }
 
     return 0;
-
 }
