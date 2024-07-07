b14c63880df0edfa204e3cc7fbbbf7589ce71220
Sudhanshu <125084256+Sudhaanshuu@users.noreply.github.com>
2024-07-07 20:45:03 +0530

    Fix problem

--- C_Code/Pointers/sizePointer.c
+++ C_Code/Pointers/sizePointer.c
@@ -1,19 +1,19 @@
-#include<STDIO.H>
-// & C program for the cheking the size of the int pointer
-struct sizePtr{};
-void values(int a,int b){
-
+#include <stdio.h>
+// C program for checking the size of the int pointer
+struct sizePtr {
+    int a;
 };
 
+void values(int a, int b) {
+}
 
-int main(int argc, char const *argv[])
-{
-    int s=10;
-    char k='G';
+int main(int argc, char const *argv[]) {
+    int s = 10;
+    char k = 'G';
     struct sizePtr sp;
 
-    int* ptr_int= &s; 
-    
-    printf("%d",sizeof(ptr_int));
+    int *ptr_int = &s;
+
+    printf("Size of int pointer: %zu\n", sizeof(ptr_int));
     return 0;
 }
