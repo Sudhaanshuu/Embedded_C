014d1a62e7a0430570a88dfe26bfaa75f7d9ce82
Kritika Prasad <ilovekripikritika@gmail.com>
2024-07-08 13:42:20 +0530

    Update tyoecaste.c
    Improve the code qulity

--- C_Code/Basic/tyoecaste.c
+++ C_Code/Basic/tyoecaste.c
@@ -1,13 +1,18 @@
-#include<STDIO.H>
-//? Simplest c program for the typecsating 
-int main(int argc, char const *argv[])
-{
-    int s=5;
-    float k=6.32;
-    char p=3;
-    s=k+p;
+#include <stdio.h>
 
-    printf("%d\n %f\n %c\n ",s,k,p);
+// Simplest C program for typecasting
+int main(int argc, char const *argv[]) {
+    int integer_value = 5;
+    float float_value = 6.32;
+    char char_value = 3;
+
+    // Perform typecasting and arithmetic operation
+    integer_value = (int)float_value + (int)char_value;
+
+    // Print the results
+    printf("Integer value: %d\n", integer_value);
+    printf("Float value: %.2f\n", float_value);
+    printf("Char value: %c (as integer: %d)\n", char_value, char_value);
 
     return 0;
 }
